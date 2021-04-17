03dc0fefe97c9aae1403109524aa4dfdd632d335
eeehh <lachlan.walls1@gmail.com>
2021-04-17 22:25:01 +1000

    feat(KettuImage): add feedback options

--- src/structures/KettuImage.js
+++ src/structures/KettuImage.js
@@ -93,6 +93,36 @@ class KettuImage {
   edit(data) {
     return this.manager.client.api.images[this.category](this.id).patch(data);
   }
+
+  /**
+   * Data representing an image flag
+   * @typedef {Object} KettuImageFlagData
+   * @property {Snowflake} user User adding the flag
+   * @property {string} reason Reason for the flag
+   */
+
+  /**
+   * Response from adding image feedback
+   * @typedef {Object} KettuImageFeedbackResponse
+   * @property {Object} likes Information for added likes
+   * @property {Array<Snowflake>} likes.added The likes that were added
+   * @property {Array<Snowflake>} likes.skipped The likes that were skipped (user already had a like)
+   * @property {Array<Snowflake>} dislikes.added The dislikes that were added
+   * @property {Array<Snowflake>} dislikes.skipped The dislikes that were skipped (user already had a dislike)
+   * @property {Array<KettuImageFlagData>} flags.added The flags that were added
+   * @property {Array<KettuImageFlagData>} flags.skipped The flags that were skipped (user already had a flag)
+   */
+
+  /**
+   * Adds feedback for the image.
+   * @param {Array<Snowflake>} likes Users to add likes for
+   * @param {Array<Snowflake>} dislikes Users to add dislikes for
+   * @param {Array<KettuImageFlagData>} flags Flags to add
+   * @returns {Promise<KettuImageFeedbackResponse>}
+   */
+  addFeedback(likes = [], dislikes = [], flags = []) {
+    return this.manager.client.api.images[this.category](this.id).feedback.patch({ data: { likes, dislikes, flags } });
+  }
 }
 
 module.exports = KettuImage;
